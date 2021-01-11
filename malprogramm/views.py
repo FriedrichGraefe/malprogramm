07df932ecd1c20fb7ba315d9860b139186db8108
frieder <friedrich.graefe@hs-augsburg.de>
2021-01-11 11:20:29 +0100

    user_id und image id funktionieren jetzt. Kommentare wurden ergänzt

--- malprogramm/views.py
+++ malprogramm/views.py
@@ -5,8 +5,6 @@ import base64
 from malprogramm.models import Image, User
 
 
-
-
 @app.route('/')
 @login_required
 def overview():
@@ -19,25 +17,24 @@ def malen():
 
         # Daten kommen hier an und werden in data gespeichert.
         data = request.get_json()
+
+        canvasdata = data.get('cdata')
+        userid = data.get('userid')
+        imagename = data.get('imgname')
+
         # Unnötiger Anfang vom String wird abgeschnitten.
-        data = data.split(",", 1)[1]
-        # Der Bildname ist am Ende des Strings und wird separat in imagename gespeichert.
-        imagename = data.split("Bildtitel", 1)[1]
+        canvasdata = canvasdata.split(",", 1)[1]
         print(imagename)
         saveimage = './malprogramm/images/' + imagename + '.png'
-        print(data)
-
-        # Hier soll aus der Datenbank Image Tabelle die letzte ID geholt werden.
-
-        # Hier fehlt der Befehl um an die angemeldete user_id zu kommen.
+        print(canvasdata)
 
         # Hier wird das Bild in der Image Tabelle gespeichert.
-        image = Image(filename=imagename, user_id=1)
+        image = Image(filename=imagename, user_id=userid)
         db.session.add(image)
         db.session.commit()
 
         # Hier wird das Bild im Ordnerverzeichnis gespeichert
-        picture_data = base64.b64decode(data)
+        picture_data = base64.b64decode(canvasdata)
         with open(saveimage, 'wb') as f:
             f.write(picture_data)
         return redirect('/', code=303)
